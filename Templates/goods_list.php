88cdacc253a7bc6a725d83bc81ec3f686072471a
Serge <s.sidorov@asu.edu.ru>
2023-05-30 14:16:19 +0400

    Добавлен дизайн шаблонов

--- Templates/goods_list.php
+++ Templates/goods_list.php
@@ -1,4 +1,17 @@
-<?
-
+<div class="wrapper">
+    <?
 foreach ($goods_list as $good)
-    echo '<p>'.$good['name'].'</p>';
+    echo '
+    <div class="card">
+        <div class="card_img">
+            <img src="images/'.$good['image'].'">
+        </div>
+        <div class="card_price">
+            <span>'.$good['price'].'$</span>
+        </div>
+        <div class="card_title">
+            '.$good['name'].'
+        </div>
+    </div>' ?>
+</div>
+
--- Templates/one_good.php
+++ Templates/one_good.php
@@ -1,10 +1,40 @@
-<h1> <? echo $good['name'] ?></h1>
+<div class="wrapper_good card">
+    <div class="img_good">
+        <img src="images/img1.webp">
+    </div>
+    <div class="info">
+        <div class="card_title">
+            <? echo $good['name'] ?>
+        </div>
+        <div class="card_price">
+            <span><? echo $good['price'].'$' ?></span>
+        </div>
+    </div>
+    <div class="comms">
+        <div class="comms_list">
+            <?
 
-<?
-foreach ($comments as $comm)
-{
-    echo '<h3>' . $comm['nickname'] . '</h3>';
-    echo '<p>' . $comm['text'] . '</p>';
-}
+            foreach ($comments as $comm) echo '<div class="comm">
+                <div class="nickname">
+                    '.$comm['nickname'].'
+                </div>
+                <div class="text">
+                    '.$comm['text'].'
+                </div>
+            </div>'
 
-?>
+            ?>
+        </div>
+        <div class="comms_add">
+            <div class="form_item">Оставить отзыв</div>
+            <div class="form_item"><input type="text" id="nickname" placeholder="Имя"></div>
+            <div class="form_item"><textarea type="text" id="text" placeholder="Отзыв" maxlength="140"></textarea></div>
+            <div class="form_item" id="msg">
+                Спасибо за отзыв!
+            </div>
+            <div class="form_item">
+                <button type="button">Отправить</button>
+            </div>
+        </div>
+    </div>
+</div>
