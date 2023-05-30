46dca764663c0b853cb91111e4b7fca0dc75bf42
Serge <s.sidorov@asu.edu.ru>
2023-05-30 10:16:49 +0400

    Создан класс модели, добавлены конструктор и метод на извлечение всех товаров

---
+++ Models/Goods.php
@@ -0,0 +1,37 @@
+<?
+
+class Goods
+{
+    private $db;
+
+    function __construct()
+    {
+        $db_name = 'admin_serge_db';
+        $db_username = 'admin_serge';
+        $db_password = 'df4zRgm3fjw';
+
+        try
+        {
+            $this->db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8;',
+                $db_username,
+                $db_password);
+        } catch (PDOException $e)
+        {
+            die();
+        }
+    }
+
+    function get_all($count = 10)
+    {
+        $result = array();
+
+        $stmt = $this->db->prepare('SELECT * FROM `goods` LIMIT :count');
+        $stmt->bindParam(':count', $count, PDO::PARAM_INT);
+        $stmt->execute();
+        
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+            $result[] = $row;
+
+        return $result;
+    }
+}
