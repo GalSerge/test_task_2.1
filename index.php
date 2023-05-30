8bdb4499deb0c33c0cb2da385fa26388e3f077bd
Serge <s.sidorov@asu.edu.ru>
2023-05-30 10:41:17 +0400

    Добвалена обработка маршрута для вывода товара

--- index.php
+++ index.php
@@ -5,17 +5,27 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require "Models/Goods.php";
+require "Views/View.php";
 
 $goods = new Goods();
+$view = new View();
 
 if (isset($_GET['good']))
 {
-    echo 1;
+    $good = $goods->get_by_id($_GET['good']);
+    
+    if ($good == null)
+    {
+        header('HTTP/1.1 404 Not Found');
+        die();
+    }
+
+    $view->show_one_good($good);
 } else if (isset($_GET['comment']))
 {
     echo 2;
 } else
 {
     $goods_list = $goods->get_all();
-    var_dump($goods_list);
+    $view->show_list($goods_list);
 }
